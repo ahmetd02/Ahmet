6a5d85ec3f1007d26ed098e88374b2f8865449b9
Ahmet <demirkolahmet02@gmail.com>
2023-01-14 23:32:56 +0300

    Ahmet local 1

--- src/Cars_2/Cars2.java
+++ src/Cars_2/Cars2.java
@@ -2,6 +2,6 @@ package Cars_2;
 
 public class Cars2 {
     public static void main(String[] args) {
-        System.out.println("Ben cars 2 clasıyım");
+        System.out.println("Ben cars 2 clasıyım.");
     }
 }
